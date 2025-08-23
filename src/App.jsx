de5f72d6ab5fe27a35a462147b7b8f851f625e27
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-23 08:34:47 -0400

    implementing more styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 // it is a react component so it need to start with an upper case
 // because this is for this component the ame is different from the html element
 const StyledApp = styled.div`
-	background-color: orangered;
 	padding: 20px;
 `;
 
@@ -16,13 +16,26 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading as="h1">The Wild Oasis</Heading>
-				<Heading as="h2">Check in and out</Heading>
-				<Button>Check in</Button>
-				<Button>Check out</Button>
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">The Wild Oasis</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button>Check in</Button>
+							<Button variations="secondary" sizes="small">
+								Check out
+							</Button>
+						</div>
+					</Row>
 
-				<Heading as="h3">Form</Heading>
-				<Input type="number" placeholder="Number of Guests" />
+					<Row>
+						<Heading as="h3">Form</Heading>
+						<form>
+							<Input type="number" placeholder="Number of Guests" />
+							<Input type="number" placeholder="Number of Guests" />
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
