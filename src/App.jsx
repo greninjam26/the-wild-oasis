3ab7cc99e86d1f25be75926fcaf8eefe0a281310
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-22 21:27:42 -0400

    styled component props and css function

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 // it is a react component so it need to start with an upper case
-const H1 = styled.h1`
-	font-size: 30px;
-	font-weight: 600;
-	background-color: yellow;
-`;
-
+// because this is for this component the ame is different from the html element
 const StyledApp = styled.div`
 	background-color: orangered;
 	padding: 20px;
@@ -20,10 +16,12 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>The Wild Oasis</H1>
+				<Heading as="h1">The Wild Oasis</Heading>
+				<Heading as="h2">Check in and out</Heading>
 				<Button>Check in</Button>
 				<Button>Check out</Button>
 
+				<Heading as="h3">Form</Heading>
 				<Input type="number" placeholder="Number of Guests" />
 			</StyledApp>
 		</>
