0384f1bce4f9680e6bc14e857d5291d1b591eef4
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-10-13 19:09:32 -0400

    add deleteCabin function

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,23 @@ export async function getCabins() {
 
 	return data;
 }
+
+// deleting a cabin from the API(mutating) with React Query
+/**
+ * 
+ * @param {the id of the cabin that we want to delete} id 
+ * @returns this return the result table after deleting the cabin
+ */
+export async function deleteCabin(id) {
+	// this take the table form supabase matches the cabin and the input id
+	// then delete the cabin from the table
+	// if the row level security is on, need to create new policy to allow edit
+	const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+	if (error) {
+		console.error(error);
+		throw new Error("Cabins could not be loaded");
+	}
+
+	return data;
+}
