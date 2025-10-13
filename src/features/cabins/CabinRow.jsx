3e9743f7885d29176bd224b8a2206c6846d1d0a6
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-10-13 19:53:19 -0400

    implement popup notification

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
 	display: grid;
@@ -54,10 +55,10 @@ function CabinRow({ cabin }) {
 		mutationFn: deleteCabin,
 		// to refetch the data, we need to revalidate the data
 		onSuccess: () => {
-			alert("Cabin successfully deleted");
+			toast.success("Cabin successfully deleted");
 			queryClient.invalidateQueries({ queryKey: ["cabins"] });
 		},
-		onError: err => alert(err.message),
+		onError: err => toast.error(err.message),
 	});
 
 	return (
