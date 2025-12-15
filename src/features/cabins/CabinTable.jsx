47d036099bc18141c6011a361c6cc51eba20def9
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-15 15:11:56 -0500

    implement a filter function for the cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,11 +3,24 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
 	const { isLoading, cabins } = useCabins();
+	const [searchParams] = useSearchParams();
 
 	if (isLoading) return <Spinner />;
+
+	// the first time enter the page the discount value is null
+	// we short circuit it with "all"
+	const filtervalue = searchParams.get("discount") || "all";
+
+	let filteredCabins;
+	if (filtervalue === "all") filteredCabins = cabins;
+	if (filtervalue === "no-discount")
+		filteredCabins = cabins.filter(cabin => cabin.discount === 0);
+	if (filtervalue === "with-discount")
+		filteredCabins = cabins.filter(cabin => cabin.discount > 0);
 
 	return (
 		<Menus>
@@ -22,7 +35,7 @@ function CabinTable() {
 				</Table.Header>
 				{/* with Render Props Pattern we don't need to render the rows here, we can tell the Table.Body what to do */}
 				<Table.Body
-					data={cabins}
+					data={filteredCabins}
 					render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
 				/>
 			</Table>
