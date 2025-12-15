b84b613003a95ab74a7e9251c0898ab1c5c1477b
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-14 21:25:56 -0500

    modify the edit and delete cabin with Modal Window

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,18 +4,22 @@ import Modal from "../../ui/Modal";
 
 function AddCabin() {
 	return (
-		// we don't want this AddCabin component to keep track of wether or not the Modal Window is open, so we need Compound Component Pattern for this to make the Modal keep track of this itself
-		<Modal>
-			{/* we don't make this a button to keep the Modal flexiable and allow the user to pass in any button their want */}
-			<Modal.Open opens="cabin-form">
-				{/* add a button to toggle the form */}
-				<Button>Add new Cabin</Button>
-			</Modal.Open>
-			{/* this is the part of the modal window that has a form, which adds a cabin, passed in as a children props */}
-			<Modal.Window name="cabin-form">
-				<CreateCabinForm />
-			</Modal.Window>
-		</Modal>
+		<div>
+			{/* we don't want this AddCabin component to keep track of wether or not the
+			Modal Window is open, so we need Compound Component Pattern for this to make
+			the Modal keep track of this itself */}
+			<Modal>
+				{/* we don't make this a button to keep the Modal flexiable and allow the user to pass in any button their want */}
+				<Modal.Open opens="cabin-form">
+					{/* add a button to toggle the form */}
+					<Button>Add new Cabin</Button>
+				</Modal.Open>
+				{/* this is the part of the modal window that has a form, which adds a cabin, passed in as a children props */}
+				<Modal.Window name="cabin-form">
+					<CreateCabinForm />
+				</Modal.Window>
+			</Modal>
+		</div>
 	);
 }
 
