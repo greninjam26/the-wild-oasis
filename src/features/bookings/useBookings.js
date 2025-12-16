6f90cc270eedc06c7807684b2ad194e69c80e30a
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-16 15:33:55 -0500

    implement no delay in the pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+	const queryClient = useQueryClient();
 	const [searchParams] = useSearchParams();
 
 	// Filter
@@ -23,10 +25,11 @@ export function useBookings() {
 	// Pagination
 	const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+	// Query
 	const {
 		isLoading,
-    // instead of just passing the data in and destructure after the data is fetched
-    // we can just set it to {}, and it works after
+		// instead of just passing the data in and destructure after the data is fetched
+		// we can just set it to {}, and it works after
 		data: { data: bookings, count } = {},
 		// data,
 		error,
@@ -35,6 +38,25 @@ export function useBookings() {
 		queryKey: ["bookings", filter, sortBy, page],
 		queryFn: () => getBookings({ filter, sortBy, page }),
 	});
+
+	// Pre-Fetching
+	// fetch the data to the next page before hand so that user don't get a loading spinner every page
+	const pageCount = Math.ceil(count / PAGE_SIZE);
+	// check to make sure we don't fetch data for a page that doesn't exist
+	// fetch the next page
+	if (page < pageCount)
+		queryClient.prefetchQuery({
+			// this set that the query is depended on and will refetches the data when one of them changes
+			queryKey: ["bookings", filter, sortBy, page + 1],
+			queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+		});
+	// fetch the page before
+	if (page > 1)
+		queryClient.prefetchQuery({
+			// this set that the query is depended on and will refetches the data when one of them changes
+			queryKey: ["bookings", filter, sortBy, page - 1],
+			queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+		});
 
 	return { isLoading, bookings, error, count };
 	// return { isLoading, error, data };
