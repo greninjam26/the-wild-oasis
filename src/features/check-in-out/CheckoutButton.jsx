054c6d070dcc812537a8bc3ecf3e7c8340849ad0
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-18 19:34:29 -0500

    implement Today Activity on the DashBoard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,19 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
+	const { checkout, isCheckingOut } = useCheckout();
+
+	return (
+		<Button
+			variations="primary"
+			sizes="small"
+			onClick={() => checkout(bookingId)}
+			disabled={isCheckingOut}
+		>
+			Check out
+		</Button>
+	);
 }
 
 export default CheckoutButton;
