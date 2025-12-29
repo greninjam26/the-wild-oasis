c43a5dce963d9454fca011c54f3a00bf11618308
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-12-29 08:48:40 -0500

    reorganize

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import { DarkModeProvider } from "./context/DarkModeContext";
 import Account from "./pages/Account";
+import Booking from "./pages/Booking";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
+import Checkin from "./pages/Checkin";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-import NewUsers from "./pages/Users";
 import Settings from "./pages/Settings";
+import NewUsers from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
-import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
-import { DarkModeProvider } from "./context/DarkModeContext";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
